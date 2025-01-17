cf5edceb35580bbcb0f49e45e12bb43b97bd534f
Junyali <junjia.liu@pm.me>
2025-01-17 20:02:38 +0000

    Initial Commit

---
+++ src/client/java/io/github/junyali/extra_ores/EXtraOresClient.java
@@ -0,0 +1,10 @@
+package io.github.junyali.extra_ores;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EXtraOresClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/io/github/junyali/extra_ores/EXtraOresDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.junyali.extra_ores;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EXtraOresDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
