dbf0e8d4f6d0d19035b61fc62877b87abc3d8316
Junyali <junjia.liu@pm.me>
2025-01-19 14:40:08 +0000

    Add tool materials

---
+++ src/main/java/io/github/junyali/extra_ores/tool/ModToolMaterial.java
@@ -0,0 +1,9 @@
+package io.github.junyali.extra_ores.tool;
+
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.registry.tag.BlockTags;
+
+public class ModToolMaterial {
+    public static final ToolMaterial INFERNIUM_TIER = ToolMaterial.DIAMOND;
+    public static final ToolMaterial VOIDIUM_TIER = ToolMaterial.NETHERITE;
+}
