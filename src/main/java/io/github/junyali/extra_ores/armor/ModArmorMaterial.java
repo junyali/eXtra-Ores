7e47e28ed63cd06bb80bde638068b425b55c9364
Junyali <junjia.liu@pm.me>
2025-01-19 16:23:21 +0000

    Add armour materials

---
+++ src/main/java/io/github/junyali/extra_ores/armor/ModArmorMaterial.java
@@ -0,0 +1,9 @@
+package io.github.junyali.extra_ores.armor;
+
+import net.minecraft.item.equipment.ArmorMaterial;
+import net.minecraft.item.equipment.ArmorMaterials;
+
+public class ModArmorMaterial {
+    public static final ArmorMaterial INFERNIUM_TIER = ArmorMaterials.DIAMOND;
+    public static final ArmorMaterial VOIDIUM_TIER = ArmorMaterials.NETHERITE;
+}
