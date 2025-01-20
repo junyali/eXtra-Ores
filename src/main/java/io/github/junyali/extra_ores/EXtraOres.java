77ca47a9ba3d146c347e597719dd1cc418d9caa9
Junyali <junjia.liu@pm.me>
2025-01-20 12:40:54 +0000

    Add Luminitite Ore Generation :DDD

--- src/main/java/io/github/junyali/extra_ores/EXtraOres.java
+++ src/main/java/io/github/junyali/extra_ores/EXtraOres.java
@@ -2,6 +2,13 @@ package io.github.junyali.extra_ores;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.PlacedFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import io.github.junyali.extra_ores.item.ModItems;
@@ -11,10 +18,13 @@ public class EXtraOres implements ModInitializer {
 	public static final String MOD_ID = "extra-ores";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static final RegistryKey<PlacedFeature> LUMINITITE_ORE_PLACED_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE, Identifier.of(MOD_ID, "luminitite_ore_generation"));
+
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Mod started!");
 		ModItems.initialize();
 		ModBlocks.initialize();
+		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, LUMINITITE_ORE_PLACED_KEY);
 	}
 }
