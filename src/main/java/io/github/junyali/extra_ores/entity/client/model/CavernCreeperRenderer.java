ba309368cbfa44c33f10b1f90f183d6555cc805d
Junyali <junjia.liu@pm.me>
2025-01-29 10:37:53 +0000

    Banging my head on how tf to add a cavern creeper

---
+++ src/main/java/io/github/junyali/extra_ores/entity/client/model/CavernCreeperRenderer.java
@@ -0,0 +1,8 @@
+package io.github.junyali.extra_ores.entity.client.model;
+
+import io.github.junyali.extra_ores.entity.custom.CavernCreeperEntity;
+import net.minecraft.client.render.entity.MobEntityRenderer;
+
+public class CavernCreeperRenderer extends MobEntityRenderer<CavernCreeperEntity, CavernCreeperModel> {
+
+}
